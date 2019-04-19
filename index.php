e9c3a3821b4c13844b3ead588bd5b39885af6f11
Robert Hill <rhill34@mail.greenriver.edu>
2019-04-19 13:18:20 -0700

    Add htaccess and index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * Title:GRC/IT328/Dating App/index.php
+ * Author: Robert Hill III
+ * Date: 04.18.2019
+ * Code Version: V1.0
+ * Availability: http://rhill.greenriverdev.com/328/datingB/index.php
+ *
+ */
+
+//Start session
+session_start();
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+////Include static head
+//include('views/head.html');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function()
+{
+    echo"<h1>my Pets</h1><br><p><a href='order'>Order a pet</a></p>";
+//    //Display a view
+//    $view = new Template();
+//    echo $view->render('views/home.html');
+
+});
+
+//Run fat free
+$f3->run();
